af320714bb111ff3cefb359b8714c712ae11be25
lucas.martins <lucas.martins@totvs.com.br>
2019-01-17 10:54:20 -0200

    added support to amqps

--- config/amqp.go
+++ config/amqp.go
@@ -5,15 +5,28 @@ import (
   "git-devops.totvs.com.br/intera/core/log"
 )
 
+const (
+  TIMEOUT_AMQP_DEFAULT	= 5
+)
+
 func LoadAmqp() {
   EnvSingletons.Logger.Infof(log.TEMPLATE_LOAD, PACKAGE, "LoadAmqp", INIT)
 
   var err error
 
+  if EnvAmqp.Timeout == 0 {
+    EnvAmqp.Timeout = TIMEOUT_AMQP_DEFAULT
+  }
+
   var config = johdin.Config{
     Hosts:    EnvAmqp.Hosts,
     Timeout:  EnvAmqp.Timeout,
     Logger:   EnvSingletons.Logger,
+    Certs:    johdin.Certs{
+      Cacert: EnvAmqp.SSL_Cacert,
+      Cert:   EnvAmqp.SSL_Cert,
+      Key:    EnvAmqp.SSL_Key,
+    },
   }
 
   if EnvSingletons.AmqpConnection, err = johdin.Connect(config); err != nil {
