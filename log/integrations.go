d83dfb78b2b4d4650b8ce36ee020d30fbcac56ef
lucas.martins <lucas.martins@totvs.com.br>
2020-01-27 16:35:26 -0200

    log

--- log/integrations.go
+++ log/integrations.go
@@ -2,14 +2,10 @@ package log
 
 const (
   INTEGRATION_MOIRAI_API  = "moirai-api"
-  INTEGRATION_PALOALTO	  = "paloalto"
-  INTEGRATION_BIGIP	  = "bigip"
-  INTEGRATION_NUAGE	  = "nuage"
-  INTEGRATION_OPENSTACK	  = "openstack"
-  INTEGRATION_WAP	  = "wap"
   INTEGRATION_CORE	  = "core"
   INTEGRATION_GORM	  = "gorm"
   INTEGRATION_UNBOUND	  = "unbound"
   INTEGRATION_RUBRIK	  = "rubrik"
   INTEGRATION_DBAAS	  = "dbaas"
+  INTEGRATION_NSXT	  = "nsxt"
 )
